eed2ba805673c07d019de79aea0b9dd15d26e8a7
Valérian <valerian.acier@hotmail.fr>
2019-10-09 12:08:09 +0200

    TP1

--- simu/simuTP1/testValueIterationBridge.java
+++ simu/simuTP1/testValueIterationBridge.java
@@ -24,6 +24,7 @@ public class testValueIterationBridge {
 					
 					VueGridworldValue vue = new VueGridworldValue(g,a);
 					
+					
 									
 					vue.setVisible(true);
 			
--- src/agent/planningagent/ValueIterationAgent.java
+++ src/agent/planningagent/ValueIterationAgent.java
@@ -56,6 +56,36 @@ public class ValueIterationAgent extends PlanningValueAgent{
 
 	}
 	
+	
+	private double bellmanEquationOnState(Etat i, ArrayList<Action> bestActions) {
+		double bestR = -999999;
+		Map<Etat, Double> etatProba;
+		List<Action> actions = this.mdp.getActionsPossibles(i);
+		for(Action a : actions) {
+			try {
+				etatProba = this.mdp.getEtatTransitionProba(i, a);
+				double r = 0;
+				for(Etat j : etatProba.keySet()) {
+					r += etatProba.get(j) * (this.mdp.getRecompense(i, a, j) + this.gamma * this.V.get(j));
+				}
+				if(r > bestR) {
+					bestR = r;
+					bestActions.clear();
+					bestActions.add(a);
+				}
+				else if(r == bestR) {
+					bestActions.add(a);
+				}
+				
+			} catch (IllegalActionException e) {
+				e.printStackTrace();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return bestR;
+	}
+	
 	/**
 	 * 
 	 * Mise a jour de V: effectue UNE iteration de value iteration (calcule V_k(s) en fonction de V_{k-1}(s'))
@@ -70,6 +100,36 @@ public class ValueIterationAgent extends PlanningValueAgent{
 		//Dans cette classe, il  faut juste mettre a jour delta 
 		this.delta=0.0;
 		//*** VOTRE CODE
+		this.vmax = 0;
+		this.vmin = 999999;
+		
+		HashMap<Etat, Double> tmpV = new HashMap<Etat,Double>();
+		for (Etat i : this.V.keySet()) {
+			
+			if(this.mdp.estAbsorbant(i)) {
+				tmpV.put(i, 0.0);
+				continue;
+			}
+			
+			//Best actions not used here but required
+			ArrayList<Action> bestActions = new ArrayList<Action>();
+			double bestR = bellmanEquationOnState(i, bestActions);
+			
+				
+			
+			if(bestR > this.vmax)
+				 this.vmax = bestR;
+			if(bestR < this.vmin)
+				this.vmin = bestR;
+			
+		
+			tmpV.put(i, bestR);
+			double tmpDelta = tmpV.get(i) - this.V.get(i);
+			if(tmpDelta > this.delta)
+				this.delta = tmpDelta;
+		}
+		
+		this.V = tmpV;
 		
 		
 		//mise a jour de vmax et vmin utilise pour affichage du gradient de couleur:
@@ -89,8 +149,12 @@ public class ValueIterationAgent extends PlanningValueAgent{
 	@Override
 	public Action getAction(Etat e) {
 		//*** VOTRE CODE
+		List<Action> actions = this.getPolitique(e);
+		if (actions.size()==0)
+			return Action2D.NONE;
+		int r = rand.nextInt(actions.size());//Random action from best actions
 		
-		return Action2D.NONE;
+		return actions.get(r);
 		
 	}
 
@@ -101,7 +165,7 @@ public class ValueIterationAgent extends PlanningValueAgent{
                  //(la valeur est calculee dans updateV
 		//*** VOTRE CODE
 		
-		return 0.0;
+		return this.V.get(_e);
 	}
 	/**
 	 * renvoi action(s) de plus forte(s) valeur(s) dans etat 
@@ -113,9 +177,11 @@ public class ValueIterationAgent extends PlanningValueAgent{
 		
 		// retourne action de meilleure valeur dans _e selon V, 
 		// retourne liste vide si aucune action legale (etat absorbant)
-		List<Action> returnactions = new ArrayList<Action>();
-	
-		return returnactions;
+		ArrayList<Action> bestA = new ArrayList<Action>();
+		this.bellmanEquationOnState(_e, bestA);
+		
+		return bestA;
+		//return returnactions;
 		
 	}
 	
@@ -124,6 +190,9 @@ public class ValueIterationAgent extends PlanningValueAgent{
 		super.reset();
                 //reinitialise les valeurs de V 
 		//*** VOTRE CODE
+		for (Etat etat:this.mdp.getEtatsAccessibles()){
+			V.put(etat, 0.0);
+		}
 		
 		this.notifyObs();
 	}
